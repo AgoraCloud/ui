290667ee9b1ba1828de825272ebfb7d465436872
Said Ghamra <35788699+saidghamra@users.noreply.github.com>
2021-02-20 20:20:16 -0500

    Merge Waleed (#32)
    * #12 started work on side menu and app bar(user can sign out now)
    
    * #12 fixed the position of the account icon
    
    * #12 abstracted the app bar and side bar to be used in other pages in future
    
    * #12 fix appbar abstraction
    
    * #28 implemented 'create workspace' form
    
    * #12 and #28 : split up top bar and side bar, added validators for createWorkspaceForm, cleaned up and abstracted code
    
    * #12 fixed abstraction of the side menu and app bar, fixed the position of workspace component on the app bar
    
    * #28 finished implementation of create workspace functionality, user can create workspaces now
    
    * #28 fix create workspace implementation
    
    * #28 fixed create workspace implementation
    
    * #28 fix the create workspace form
    
    Co-authored-by: waleed zafar <waleedza99@gmail.com>

---
+++ src/app/components/SideBar/CreateWorkspaceDialog/index.tsx
@@ -0,0 +1,135 @@
+import * as React from 'react';
+import { observer, inject } from 'mobx-react';
+import { WORKSPACES_STORE } from 'app/constants'
+import { WorkspacesStore } from 'app/stores';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+// form dialogue 
+import Button from '@material-ui/core/Button';
+import { Input } from 'app/components/Inputs';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import { Typography } from '@material-ui/core';
+
+// icons
+import MemoryIcon from '@material-ui/icons/Memory';
+import MoneyIcon from '@material-ui/icons/Money';
+import StorageIcon from '@material-ui/icons/Storage';
+
+
+const useStyles = makeStyles((theme) => ({
+    margin: {
+        margin: theme.spacing(1, 0, 1, 0),
+    },
+    subtitle: {
+        margin: theme.spacing(1, 0, 1, 0),
+        color: "inherit",
+        variant: "subtitle1",
+    }
+}));
+
+export const CreateWorkspaceDialog = inject(WORKSPACES_STORE)(observer((props) => {
+    const store = props[WORKSPACES_STORE] as WorkspacesStore
+    const form = store.createWorkspaceForm
+    const classes = useStyles();
+
+    const handleFormSubmission = async () => {
+        const success = await store.createWorkspace()
+        success && props.closeForm()
+    }
+
+    // console.log("Check this out")
+    // console.log(form)
+    return <Dialog open={props.formOpen} onClose={props.closeForm} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Create Workspace</DialogTitle>
+        <DialogContent>
+            <DialogContentText>
+                Please fill out the form below and press 'Create' to create a workspace.
+            </DialogContentText>
+            <Input
+                autoFocus
+                form={form}
+                className={classes.margin}
+                margin="dense"
+                id="name"
+                label="Workspace Name"
+                type="text"
+                fullWidth
+            />
+            <Typography className={classes.subtitle} >Resources</Typography>
+            <DialogContentText>
+               Optionally, you can specify the maximum amount of resources the workspace can use:
+            </DialogContentText>
+            <Input
+                form={form}
+                className={classes.margin}
+                margin="dense"
+                id="properties.resources.cpuCount"
+                label="CPU"
+                type="number"
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <MemoryIcon />
+                        </InputAdornment>
+                    ),
+                }}
+                InputLabelProps={{
+                    required: false,
+                }}
+                fullWidth
+            />
+            <Input
+                form={form}
+                className={classes.margin}
+                margin="dense"
+                id="properties.resources.memoryCount"
+                label="RAM"
+                type="number"
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <MoneyIcon />
+                        </InputAdornment>
+                    ),
+                }}
+                InputLabelProps={{
+                    required: false,
+                }}
+                fullWidth
+            />
+            <Input
+                form={form}
+                className={classes.margin}
+                margin="dense"
+                id="properties.resources.storageCount"
+                label="Storage"
+                type="number"
+                InputProps={{
+                    startAdornment: (
+                        <InputAdornment position="start">
+                            <StorageIcon />
+                        </InputAdornment>
+                    ),
+                }}
+                InputLabelProps={{
+                    required: false,
+                }}
+                fullWidth
+            />
+        </DialogContent>
+        <DialogActions>
+            <Button onClick={props.closeForm} color="primary">
+            Cancel
+            </Button>
+            <Button onClick={handleFormSubmission} disabled={!form.isValid} color="primary">
+            Create
+            </Button>
+        </DialogActions>
+    </Dialog>
+}))
