28ed33990147446795af33b619ffeeabb061e24f
Said Ghamra <s.ghamra@hotmail.com>
2021-04-07 12:13:48 -0400

    Fixed merge issue

--- src/app/components/Kanban/board-item.tsx
+++ src/app/components/Kanban/board-item.tsx
@@ -1,29 +1,29 @@
-import * as React from 'react'
-import { Draggable } from 'react-beautiful-dnd'
-import styled from 'styled-components'
-import Divider from '@material-ui/core/Divider';
-import { observer } from 'mobx-react'
-import { CustomCard } from './Card'
+import * as React from "react";
+import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+import Divider from "@material-ui/core/Divider";
+import { observer } from "mobx-react";
+import { CustomCard } from "./card";
 
 // Define types for board item element properties
 type BoardItemProps = {
-  index: number
-  item: any
-  laneId: string
-}
+  index: number;
+  item: any;
+  laneId: string;
+};
 
 // Define types for board item element style properties
 // This is necessary for TypeScript to accept the 'isDragging' prop.
 type BoardItemStylesProps = {
-  isDragging: boolean
-}
+  isDragging: boolean;
+};
 
 // Create style for board item element
 const BoardItemEl = styled.div<BoardItemStylesProps>`
   padding: 8px;
-  background-color: ${(props) => props.isDragging ? '#d3e4ee' : 'none'};
+  background-color: ${(props) => (props.isDragging ? "#d3e4ee" : "none")};
   border-radius: 4px;
-  transition: background-color .25s ease-out;
+  transition: background-color 0.25s ease-out;
 
   &:hover {
     background-color: #2196f3;
@@ -32,24 +32,31 @@ const BoardItemEl = styled.div<BoardItemStylesProps>`
   & + & {
     margin-top: 4px;
   }
-`
+`;
 
 // Create and export the BoardItem component
 export const BoardItem = (props: BoardItemProps) => {
-  return <Draggable draggableId={props.item.id} index={props.index}>
-    {(provided, snapshot) => (
-      <BoardItemEl
-        {...provided.draggableProps}
-        {...provided.dragHandleProps}
-        ref={provided.innerRef}
-        isDragging={snapshot.isDragging}
-      >
-        {/* The content of the BoardItem */}
-        {/* {"Title: " + props.item.title}
+  return (
+    <Draggable draggableId={props.item.id} index={props.index}>
+      {(provided, snapshot) => (
+        <BoardItemEl
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          ref={provided.innerRef}
+          isDragging={snapshot.isDragging}
+        >
+          {/* The content of the BoardItem */}
+          {/* {"Title: " + props.item.title}
         <Divider style={{marginTop: "10px", marginBottom: "10px"}} />
         {"Description: " + props.item.description} */}
-        <CustomCard id={props.item.id} title={props.item.title} description={props.item.description} laneId={props.laneId} />
-      </BoardItemEl>
-    )}
-  </Draggable>
-}
+          <CustomCard
+            id={props.item.id}
+            title={props.item.title}
+            description={props.item.description}
+            laneId={props.laneId}
+          />
+        </BoardItemEl>
+      )}
+    </Draggable>
+  );
+};
