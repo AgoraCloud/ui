0aa0f1abe228cd20844a78ce9eb0276f53a08a94
mframe <mframe45@gmail.com>
2021-10-28 15:22:48 -0400

    logs typo

--- src/app/res/Deployments/models/logs.ts
+++ src/app/res/Deployments/models/logs.ts
@@ -1,4 +1,4 @@
-import { DeploymentModel } from '.';
+import { DeploymentModel } from 'app/res/Deployments';
 import { APIRepo, Model, OnDemandRepo } from '@mars-man/models';
 
 export class DeploymentLogsModel extends Model<string> {
@@ -6,7 +6,9 @@ export class DeploymentLogsModel extends Model<string> {
     super({
       data: '',
     });
-    this.repos = OnDemandRepo(new APIRepo({ path: this.api }));
+    this.repos = {
+      main: OnDemandRepo(new APIRepo({ path: this.api }))
+    }
   }
 
   get logs() {
