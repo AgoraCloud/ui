d001c1d4bf996521cd06e961a664cba6ecee478d
mframe <mframe45@gmail.com>
2021-11-14 20:12:55 -0500

    fixed workspace metrics

--- src/app/res/Workspaces/views/Metrics.tsx
+++ src/app/res/Workspaces/views/Metrics.tsx
@@ -25,12 +25,16 @@ export const WorkspaceMetricsPage = observer((props) => {
   const metrics = workspace?.metrics;
 
   // if(metrics?.state != 'loaded') return null
-  return (
-    <WorkspaceWrapper>
+  if (metrics.state === 'error') {
+    return <WorkspaceWrapper>
       <Typography variant="h4">{workspace.name} Metrics</Typography>
       <br />
       <WorkspaceAlert metrics={metrics} />
-      <br />
+    </WorkspaceWrapper>
+  }
+  return (
+    <WorkspaceWrapper>
+      <Typography variant="h4">{workspace.name} Metrics</Typography>
       <Grid container spacing={4}>
         <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
           <GaugeChart {...metrics.cpuChart} />
