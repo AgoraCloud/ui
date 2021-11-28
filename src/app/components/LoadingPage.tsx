a34eb17c172a17de55c3e32a85e95b42f7d3a6e4
mframe <mframe45@gmail.com>
2021-11-28 14:08:37 -0500

    added loading page

--- src/app/components/LoadingPage.tsx
+++ src/app/components/LoadingPage.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import LoadingLogo from './LoadingLogo.svg'
 
-export const LoadingPage = () => {
-  return <div>Loading...</div>;
-};
+
+export const LoadingText = () => {
+  const [count, setCount] = React.useState(0)
+  setInterval(()=>{
+    setCount((count + 1)%4)
+  }, 500)
+
+  return <div style={{
+    fontSize: "30px",
+    position: "absolute",
+    left: "calc(50% - 60px)",
+  }}>{"Loading" + ".".repeat(count)}</div>
+}
+
+export const LoadingPage = React.memo(() => {
+  return <div>
+    {/* <img src={LoadingLogo}/> */}
+    {/* {LoadingLogo} */}
+    <object type="image/svg+xml" style={{height: "80vh", width: "80vw", marginTop: "10vh", marginLeft: "10vw",}} data={LoadingLogo}>svg-animation</object>
+    <LoadingText/>
+  </div>
+});
